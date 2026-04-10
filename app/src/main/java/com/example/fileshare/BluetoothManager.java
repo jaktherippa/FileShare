915c46368e13aa962ae403d57ff214590ec094c7
jaktherippa <andrewchemwa@gmail.com>
2026-04-10 13:45:03 +0300

    setting up bluetooth manager

--- app/src/main/java/com/example/fileshare/BluetoothManager.java
+++ app/src/main/java/com/example/fileshare/BluetoothManager.java
@@ -1,12 +1,18 @@
 package com.example.fileshare;
 
-import static androidx.core.app.ActivityCompat.startActivityForResult;
-
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.content.Intent;
-import android.widget.Button;
-
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 public class BluetoothManager{
+    private ActivityResultLauncher<Intent> enableBluetoothLauncher = new ActivityResultContracts.StartActivityForResult(), result -> {
+        if(result.getResultCode() == Activity.RESULT_OK){
+            //Bluetooth was turned on successfully
+        }else{
+            //Bluetooth Not Supported
+        }
+    }
     public BluetoothManager(ConnectionSetupActivity activity){
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if(mBluetoothAdapter == null){
@@ -14,7 +20,7 @@ public class BluetoothManager{
         }
         if (mBluetoothAdapter != null && !mBluetoothAdapter.isEnabled()) {
             Intent bluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(activity,bluetooth,0, " ");
+
         }
     }
 }
